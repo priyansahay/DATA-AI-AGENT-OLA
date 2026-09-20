232cb6aa9aa00ab759616741370c68b576e9a77a
Priyanshu Sahay <priyanshusahay489@gmail.com>
2026-09-21 00:31:27 +0530

    Data added successfully

--- agents/sql_analyst.py
+++ agents/sql_analyst.py
@@ -9,8 +9,15 @@ from Models.schema import AgentSchema
 def curate_ques(state: AgentSchema) -> AgentSchema:
     user_question = state.user_ques
     llm = pick_llm("low")
-    response = llm.invoke(f"Curate the following Question {user_question} to make it more clear and concise.")
+    response = llm.invoke(f"Curate the following Question: {user_question}.")
     state.curated_ques = response
+    return state
+
+def prompt_query_context(state: AgentSchema) -> AgentSchema:
+    curated_question = state.curated_ques
+    llm = pick_llm("medium")
+    response = llm.invoke(f"Generate a detailed prompt with SQL DB context for the following curated question: {curated_question}.")
+    state.prompt_query_context = response
     return state
 
 
